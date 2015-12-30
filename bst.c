eb522b57690e8a1ec8022c3e1dab5bbf1b00eed8
Vicky210 <vigneshgm210@gmail.com>
2015-12-29 20:12:18 -0500

    Added Find Min & Max in BST

--- bst.c
+++ bst.c
@@ -29,7 +29,52 @@ bool Search(struct Node* root,int x){
    else
 	return Search(root->right,x);
 }
-
+int FindMin(struct Node* root){
+   if(root == NULL){
+	printf("\nThe tree is empty\n");
+	return 0;
+   }
+   struct Node* temp = root;
+   while(temp->left != NULL)	
+	temp = temp->left;
+   printf("\nthe minimum value is : %d\n",temp->data);
+   return 0;
+}
+int FindMax(struct Node* root){
+   if(root == NULL){
+        printf("\nThe tree is empty\n");
+        return 0;
+   }
+   struct Node* temp = root;
+   while(temp->right != NULL)
+        temp = temp->right;
+   printf("\nthe maximum value is : %d\n",temp->data);
+   return 0;
+}
+int FindMin_rec(struct Node* root){
+   if(root == NULL){
+        printf("\nThe tree is empty\n");
+        return 0;
+   }
+   else if(root->left == NULL){
+	printf("\nthe minimum value is : %d\n",root->data);
+	return 0;
+   }
+   else
+	return FindMin_rec(root->left);
+}
+int FindMax_rec(struct Node* root){
+   if(root == NULL){
+        printf("\nThe tree is empty\n");
+        return 0;
+   }
+   else if(root->right == NULL){
+        printf("\nthe maximum value is : %d\n",root->data);
+        return 0;
+   }
+   else
+        return FindMax_rec(root->right);
+}
 
 void main(){
    struct Node* root = NULL;
